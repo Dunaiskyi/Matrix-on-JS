51eed0b972e476b2db511802c848313b1658823c
Dima Dunayskiy <dima0dunayskiy@gmail.com>
2017-04-02 22:05:29 +0300

    matrix html document

---
+++ js/common.js
@@ -0,0 +1,90 @@
+'use strict'
+
+var matrix = document.getElementById('matrix')
+var ctx = matrix.getContext('2d')
+var op = 1
+
+var config = {
+  amount: 0,  // of columns going down (it will top at this number)
+  speed: 80,   // time between updates in ms (lower = faster)
+  size: 15,    // in px
+  minLength: 40,
+  maxLength: 50,
+  firstColor: '#fff',
+  color: 'rgba(0, 255, 32, 1)',
+  density: 5
+}
+
+var datarray = []
+
+var width = ctx.canvas.width = window.innerWidth
+var height = ctx.canvas.height = window.innerHeight
+
+/*
+Data colum object
+=============
+*/
+function Data(x) {
+  this.x = x
+  this.y = 0
+  this.history = []
+  this.historySizeMax = Math.floor(Math.random() * config.maxLength + config.minLength)
+}
+
+Data.prototype = {
+  update() {
+    this.y += config.size
+    if (this.y >= height + this.historySizeMax * config.size) {
+      datarray.splice(datarray.indexOf(this), 1)
+      putData()
+    }
+
+    this.history.unshift(String.fromCharCode(32 + Math.floor(Math.random() * 90)))
+    if (this.history.length > this.historySizeMax) this.history.pop()
+  },
+
+  draw() {
+    ctx.fillStyle = config.firstColor
+    ctx.fillText(this.history[0], this.x, this.y)
+    ctx.fillStyle = config.color
+    op = 1
+
+    for (var i = 1, char; char = this.history[i]; i++) {
+      ctx.fillStyle = 'rgba(0, 255, 32,' + (op-= 1/config.maxLength) + ')'
+      ctx.fillText(char, this.x, this.y - i * config.size)
+    }
+  }
+
+}
+
+var count = Math.floor(width / config.size)
+
+function putData() {
+  var newX = Math.floor(Math.random() * count) * config.size
+
+  for (var i = 0, row; row = datarray[i]; i++) {
+    if(row.x == newX && row.y - row.historySizeMax * config.size + config.size < config.size) return
+  }
+  datarray.push(new Data(newX))
+}
+
+/*
+Init & loop
+=============
+*/
+ctx.font = config.size + 'px monospace'
+ctx.shadowOffsetX = 1
+ctx.shadowOffsetY = 1
+ctx.shadowBlur = 6
+ctx.shadowColor = config.color
+
+setInterval(function() {
+  ctx.clearRect(0, 0, width, height)
+  for (var a = 0; a < config.density; a++) {
+    if (config.amount == 0 || datarray.length < config.amount) putData()
+  }
+    for (var i = 0, column; column = datarray[i]; i++) {
+      column.update()
+      column.draw()
+    }
+}, config.speed)
